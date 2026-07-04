000bb3f1b8c855bc304aaf2bfa1e7f1dd38c1d9e
Farhan <farhan7534031b@gmail.com>
2026-07-04 07:56:19 +0700

    Link Windows resources into app package

--- scripts/build-windows.ps1
+++ scripts/build-windows.ps1
@@ -6,8 +6,12 @@ go run .\tools\write-icon
 if (-not (Get-Command go-winres -ErrorAction SilentlyContinue)) {
   go install github.com/tc-hib/go-winres@latest
 }
+Remove-Item .\cmd\whispr\rsrc_windows_*.syso -ErrorAction SilentlyContinue
+Remove-Item .\rsrc_windows_*.syso -ErrorAction SilentlyContinue
+
 go-winres make
 if ($LASTEXITCODE -ne 0) { throw "go-winres failed" }
+Move-Item .\rsrc_windows_*.syso .\cmd\whispr\ -Force
 
 go build -ldflags="-H windowsgui" -o dist\whispr.exe .\cmd\whispr
 if ($LASTEXITCODE -ne 0) { throw "go build failed" }
